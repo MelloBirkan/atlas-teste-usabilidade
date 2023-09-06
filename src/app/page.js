d2263f24e493c2a8e4facbf72aae61c153f9acfb
Mello Birkan <mgbirkan@icloud.com>
2023-09-06 16:33:33 -0300

    `.eslintrc` Adicionado para o projeto

--- src/app/page.js
+++ src/app/page.js
@@ -5,33 +5,34 @@ export default function Home() {
     <div>
       <h1>Atlas das Américas: Avaliação de usabilidade</h1>
       <p>
-        A avaliação de usabilidade busca entender como os 
-        usuários interagem com o produto, identificar problemas de 
-        usabilidade, e coletar feedbacks para melhorar a experiência 
-        do usuário. Essa avaliação será realizada em um protótipo, ou seja, 
-        é uma versão inicial e simplificada do produto final, criada 
-        especificamente para testar ideias e funcionalidades-chave.
-        </p>
-        <p>
-        Abaixo, temos as tarefas que serão realizadas e 
-        testadas por você no protótipo. Ao final de 
-        cada tarefa, olhe a próxima tarefa antes de 
-        realizar qualquer ação. 
-        </p>
-        <p>
-        Após realizar todas as tarefas, 
-        temos outro link para o preenchimento de um questionário 
-        pós teste com o propósito de entender sua experiência e 
-        coletar feedbacks. Desde já, agradecemos sua colaboração! 
-        </p>
-        <h2>Protrótipo</h2>
-        <p>
-          Por favor, clique no seguinte link para acessar o 
-          protótipo e com ele aberto, faça as tarefas: 
-        </p>
-        <a href='https://www.figma.com/proto/mI8jPFOK39MSB1AyiQmkOT/Atlas-prototipo?page-id=0%3A1&type=design&node-id=461-13978&viewport=-6628%2C-5350%2C0.34&t=RXELGDCSVspqvtg0-1&scaling=scale-down&starting-point-node-id=461%3A13978&mode=design' target='_blank'>
-          <span>Clique aqui para entrar no protótipo</span>
-          </a>
+        A avaliação de usabilidade busca entender como os usuários interagem com
+        o produto, identificar problemas de usabilidade, e coletar feedbacks
+        para melhorar a experiência do usuário. Essa avaliação será realizada em
+        um protótipo, ou seja, é uma versão inicial e simplificada do produto
+        final, criada especificamente para testar ideias e
+        funcionalidades-chave.
+      </p>
+      <p>
+        Abaixo, temos as tarefas que serão realizadas e testadas por você no
+        protótipo. Ao final de cada tarefa, olhe a próxima tarefa antes de
+        realizar qualquer ação.
+      </p>
+      <p>
+        Após realizar todas as tarefas, temos outro link para o preenchimento de
+        um questionário pós teste com o propósito de entender sua experiência e
+        coletar feedbacks. Desde já, agradecemos sua colaboração!
+      </p>
+      <h2>Protrótipo</h2>
+      <p>
+        Por favor, clique no seguinte link para acessar o protótipo e com ele
+        aberto, faça as tarefas:
+      </p>
+      <a
+        href="https://www.figma.com/proto/mI8jPFOK39MSB1AyiQmkOT/Atlas-prototipo?page-id=0%3A1&type=design&node-id=461-13978&viewport=-6628%2C-5350%2C0.34&t=RXELGDCSVspqvtg0-1&scaling=scale-down&starting-point-node-id=461%3A13978&mode=design"
+        target="_blank"
+      >
+        <span>Clique aqui para entrar no protótipo</span>
+      </a>
     </div>
   )
 }
