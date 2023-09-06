37a709457fba1c26e8b6c93ee68b1468adc17933
matheusapostulo <matheusapostulo10@gmail.com>
2023-09-06 19:06:22 -0300

    site estilizado

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="flex flex-start"> 
         <h1 className="text-slate-950 text-6xl font-bold pb-4 pt-4">Atlas das Américas: Avaliação de usabilidade</h1>
       </div>
-      <div className="pb-4 text-justify">
+      <div className="text-justify">
       <p className="pb-4">
         A avaliação de usabilidade busca entender como os usuários interagem com
         o produto, identificar problemas de usabilidade, e coletar feedbacks
@@ -18,14 +18,14 @@ export default function Home() {
         protótipo. Ao final de cada tarefa, olhe a próxima tarefa antes de
         realizar qualquer ação.
       </p>
-      <p>
+      <p className="pb-0">
         Após realizar todas as tarefas, temos outro link para o preenchimento de
         um questionário pós teste com o propósito de entender sua experiência e
         coletar feedbacks. Desde já, agradecemos sua colaboração!
       </p>
       </div>
-      <h2 className="text-slate-950 text-4xl font-bold pb-2 pt-8 text-left">Protótipo</h2>
-      <p>
+      <h2 className="text-slate-950 text-4xl font-bold pb-2 pt-10 text-left">Protótipo</h2>
+      <p className="pb-1">
         Por favor, clique no seguinte link para acessar o protótipo e com ele
         aberto, faça as tarefas:
       </p>
@@ -35,38 +35,38 @@ export default function Home() {
       >
       <span className="font-bold text-blue-700 underline-4">Clique aqui para entrar no protótipo</span>
       </a>
-      <h2 className="text-slate-950 text-4xl font-bold pb-1 pt-12 text-left">Tarefas</h2>
-      <h3 className="text-lg pt-2 pb-6">Encontrar fichas</h3>
-      <ol className="list-decimal list-inside space-y-10">
-        <li className="space-y-2">
-          Encontre a Ficha Americana <span className="font-bold text-blue-700">Citação/Intimação/Notificação</span> do país <strong>Brasil</strong> 
-            <br /><strong className="text-lg text-yellow-700 m-4">Resultado esperado</strong>
+      <h2 className="text-slate-950 text-4xl font-bold pb-1 pt-10 text-left">Tarefas</h2>
+      <h3 className="text-lg pt-1 pb-4">Encontrar fichas</h3>
+      <ol className="list-inside space-y-10 ">
+        <li className="">
+          <strong>1.</strong> Encontre a Ficha Americana <span className="font-bold">Citação/Intimação/Notificação</span> do país <strong>Brasil</strong> 
+            <p className="pt-1 pb-1"><strong className="text-lg text-yellow-700 m-1">Resultado esperado</strong></p>
             <img className="w-46 h-96" src="../images/tarefa 1.png"></img>
         </li> 
         <li className="">
-          Volte para as Fichas Americanas do país <strong>Brasil</strong>
-          <br /><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong>
+          <strong>2.</strong> Volte para as Fichas Americanas do país <strong>Brasil</strong>
+          <p className="pt-1 pb-1"><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong></p>
           <img className="w-46 h-96" src="../images/tarefa 2.png"></img>
         </li>
         <li className="">
-          Encontre a Ficha Americana<span className="font-bold text-blue-700"> Citação/Intimação/Notificação</span> do país <strong>Argentina</strong>
-          <br /><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong>
+          <strong>3.</strong> Encontre a Ficha Americana<span className="font-bold "> Citação/Intimação/Notificação</span> do país <strong>Argentina</strong>
+          <p className="pt-1 pb-1"><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong></p>
           <img className="w-46 h-96" src="../images/tarefa 3.png"></img>
         </li>
         <li className="">
-          Deixe a Ficha Americana<span className="font-bold text-blue-700">Citação/Intimação/Notificação</span> do país <strong>Brasil</strong> no <strong>idioma espanhol</strong>
-          <br /><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong>
+          <strong>4.</strong> Deixe a Ficha Americana<span className="font-bold "> Citação/Intimação/Notificação</span> do país <strong>Brasil</strong> no <strong>idioma espanhol</strong>
+          <p className="pt-1 pb-1"><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong></p>
           <img className="w-46 h-96" src="../images/tarefa 4.png"></img>
         </li>
         <li className="my-4">
-        Volte a deixar a página da Ficha Americana<span className="font-bold text-blue-700">Citação/Intimação/Notificação</span> do país <strong>Brasil</strong> no <strong>idioma portugues</strong>
-        <br /><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong>
+          <strong> 5.</strong> Volte a deixar a página da Ficha Americana <span className="font-bold "> Citação/Intimação/Notificação</span> do país <strong>Brasil</strong> no <strong>idioma portugues</strong>
+          <p className="pt-2 pb-1"><strong className="text-lg m-1 text-yellow-700">Resultado esperado</strong></p>
           <img className="w-46 h-96" src="../images/tarefa 3.png"></img>
         </li>
       </ol>
-      <h2 className="text-slate-950 text-4xl font-bold pb-2 pt-7 text-left">Questionário pós teste</h2>
-      <p>Obrigado por chegar até aqui!, clique no seguinte link para acessar o questionário pós teste:</p>
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdEOmA-zsTKw609oyvKEWt0ig-2S6CWhFsBNx_bGntWD9kGvQ/viewform?usp=sf_link">Link para o questionário</a>
+      <h2 className="text-slate-950 text-4xl font-bold pb-2 pt-10 text-left">Questionário pós teste</h2>
+      <p className="pb-1">Obrigado por chegar até aqui! Clique no seguinte link para acessar o questionário pós teste:</p>
+      <a className="font-bold text-blue-700 pb-16" href="https://docs.google.com/forms/d/e/1FAIpQLSdEOmA-zsTKw609oyvKEWt0ig-2S6CWhFsBNx_bGntWD9kGvQ/viewform?usp=sf_link" target="_blank">Link para o questionário</a>
     </div>
   )
 }
